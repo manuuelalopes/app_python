b1bfc75aa9a8cb562f4b62a8639056240da89064
Manuela Lopes <manulopes3301@gmail.com>
2026-03-25 09:38:46 -0300

    removendo recuo

--- app.py
+++ app.py
@@ -26,5 +26,4 @@ class Servidor(BaseHTTPRequestHandler):
          self.end_headers()
          self.wfile.write(b"POST recebido")
 
-
-         HTTPServer(("0.0.0.0", 8000), Servidor).serve_forever() 
+ HTTPServer(("0.0.0.0", 8000), Servidor).serve_forever() 
